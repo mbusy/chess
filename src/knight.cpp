bb242e20d8c276e06644585d0fc1c33dcd2f8e93
Maxime Busy <mbusy.pro@gmail.com>
2021-04-20 02:36:21 +0200

    Add motion rules for the knight

--- src/knight.cpp
+++ src/knight.cpp
@@ -23,6 +23,10 @@ void Knight::show_possible_moves(
     std::vector<sf::Vector2i> positions = this->compute_possible_moves(
         slots,
         position);
+
+    for (auto position : positions) {
+        slots[position.x][position.y].highlight(true);
+    }
 }
 
 std::vector<sf::Vector2i> Knight::compute_possible_moves(
@@ -30,6 +34,38 @@ std::vector<sf::Vector2i> Knight::compute_possible_moves(
         const sf::Vector2i& position) const {
     
     std::vector<sf::Vector2i> possible_moves;
+    std::vector<sf::Vector2i> offsets = {
+        sf::Vector2i(-2, -1),
+        sf::Vector2i(-2, 1),
+        sf::Vector2i(-1, 2),
+        sf::Vector2i(1, 2),
+        sf::Vector2i(2, 1),
+        sf::Vector2i(2, -1),
+        sf::Vector2i(1, -2),
+        sf::Vector2i(-1, -2)};
+
+    for (auto offset : offsets) {
+        auto candidate = position + offset;
+
+        if (candidate.x > 7 ||
+                candidate.x < 0 ||
+                candidate.y > 7 ||
+                candidate.y < 0) {
+
+            continue;
+        }
+
+        if (slots[candidate.x][candidate.y].status == OCCUPIED) {
+            if (slots[candidate.x][candidate.y].piece->get_piece_id() !=
+                    this->piece_id) {
+
+                possible_moves.push_back(candidate);
+            }
+        }
+        else {
+            possible_moves.push_back(candidate);
+        }
+    }
 
     return possible_moves;
 }
